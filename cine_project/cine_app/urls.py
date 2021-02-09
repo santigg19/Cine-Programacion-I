7bec5d44f8f25148555447c3a877196687288e7a
santigg19 <graffigna.santi@gmail.com>
2021-02-09 20:32:59 -0300

    extras

--- cine_project/cine_app/urls.py
+++ cine_project/cine_app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
 from cine_app import views
-from .views import MovieAPIView, MovieDetails, index, HallAPIView, HallDetails, ProjectionAPIView, ActiveProjectionAPIView, MovieTimeAPIView, movie_fechas, ProjectionTimeAPIView
+from .views import MovieAPIView, MovieDetails, index, HallAPIView, HallDetails, ProjectionAPIView, ActiveProjectionAPIView, MovieTimeAPIView, movie_time_range, ProjectionTimeAPIView, SeatAPIView, SeatsDetails, ReportSalesTimeRange, ReportProjectionTimeRange, ReportActiveMovies
 
 urlpatterns = [
 
@@ -12,9 +12,10 @@ urlpatterns = [
     path('movie/', MovieAPIView.as_view()),                     # OK Retorna    peliculas
     path('detail/<int:id_movie>/', MovieDetails.as_view()),     # OK            pelicula en particular por id
     path('movietime/', MovieTimeAPIView.as_view()),             # OK            peliculas en un rango de 8 dias
+    url(r'^moviedates/([a-zA-Z0-9 ]+)/(\d{4}[-/]\d{2}[-/]\d{2})/(\d{4}[-/]\d{2}[-/]\d{2})$', views.movie_time_range),
 
-    # Ejemplo http://localhost:8000/moviedate/DjangoUnchained/2020-12-15/2020-12-25
-    url(r'^moviedates/([a-zA-Z0-9 ]+)/(\d{4}[-/]\d{2}[-/]\d{2})/(\d{4}[-/]\d{2}[-/]\d{2})$', views.movie_fechas),
+    # Ejemplo http://localhost:8000/moviedates/DjangoUnchained/2020-12-15/2020-12-25
+    #url(r'^moviedates/([a-zA-Z0-9 ]+)/(\d{4}[-/]\d{2}[-/]\d{2})/(\d{4}[-/]\d{2}[-/]\d{2})$', views.movie_fechas),
 
     # Endpoint Sala
     path('hall/', HallAPIView.as_view()),                       # OK            salas
@@ -24,5 +25,17 @@ urlpatterns = [
     path('projections/', ProjectionAPIView.as_view()),
     path('activeprojections/', ActiveProjectionAPIView.as_view()),   # OK
     path('projectionstime/', ProjectionTimeAPIView.as_view()), # Proyecciones en un rango de 2 semanas
+
+
+    # Asientos
+    path('seat/', SeatAPIView.as_view()),     # Asientos ocupados
+    path('seatdetail/<int:id_seats>/', SeatsDetails.as_view()),  # Asiento en particular
+
+
+    # Reportes
+    path('report/salesreport/', ReportSalesTimeRange.as_view()),
+    path('report/salesprojection/<int:id_projection>', ReportProjectionTimeRange.as_view()),
+    path('report/activemovie/', ReportActiveMovies.as_view()),
+
 
 ]
--- cine_project/cine_app/views.py
+++ cine_project/cine_app/views.py
@@ -1,4 +1,7 @@
+from abc import abstractmethod
+from django.db.models.aggregates import Count
 from django.shortcuts import render
+
 
 from django.http.response import JsonResponse
 from django.http import HttpResponse
@@ -40,9 +43,6 @@ class MovieAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-
-
 class MovieTimeAPIView(APIView):
     
     def get(self, request):
@@ -60,8 +60,6 @@ class MovieTimeAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 
 
 # OK
@@ -92,6 +90,22 @@ class MovieDetails(APIView):
         movie = self.get_object(id_movie)
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET'])
+def movie_time_range(request, name, start, finish):
+
+    try:
+        name = name_spaces(name)
+        movies = Movie.objects.get(name=name, start_date=start, finish_date=finish)
+        serializer = MovieSerializer(movies)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Movie.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+
+def name_spaces(moviename):
+    return re.sub(r"(\w)([A-Z])", r"\1 \2", moviename)
 
 
 # OK
@@ -199,18 +213,110 @@ class ProjectionTimeAPIView(APIView):
         return Response(serializer.data)
 
 
-@api_view(['GET'])
-def movie_fechas(request, name, start, finish):
 
-    try:
-        name = name_spaces(name)
-        movies = Movie.objects.get(name=name, start_date=start, finish_date=finish)
-        serializer = MovieSerializer(movies)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    except Movie.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+# Asientos
 
 
-def name_spaces(moviename):
-    return re.sub(r"(\w)([A-Z])", r"\1 \2", moviename)
+class SeatAPIView(APIView):
+    
+    def get(self, request):
+        seats = Seats.objects.all()
+        serializer = SeatSerializer(seats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SeatSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SeatsDetails(APIView):
+
+    def get_object(self, id_seats):
+        try:
+            return Seats.objects.get(id_seats=id_seats)
+
+        except Seats.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id_seats):
+        seat = self.get_object(id_seats)
+        serializer = SeatSerializer(seat)
+        return Response(serializer.data)
+
+    def put(self, request, id_seats):
+        seat = self.get_object(id_seats)
+        serializer = SeatSerializer(seat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id_seats):
+        seat = self.get_object(id_seats)
+        seat.delete()
+        return Response({'SALA ELIMINADA'},status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+# Reporte de ventas en dos semanas
+class ReportSalesTimeRange(APIView):
+    
+    def get(self, request):
+        now = dt.datetime.now()
+        time_range = dt.timedelta(days=14)
+        tickets = Seats.objects.filter(date__gte=(now - time_range), date__lte=(now + time_range))
+        serializer = SeatSerializer(tickets, many=True)
+        sales = 0
+        for ticket in tickets:
+            sales += 1
+        #return Response(serializer.data)   descomentar y cambiar por el otro return si se quiere retornar en detalle los asientos
+        return Response({f'Se han vendido un total de {sales} tickets en un rango de 14 dias'})
+
+
+# Reporte de ventas en tiempo y en una determinada proyeccion
+class ReportProjectionTimeRange(APIView):
+    def get_object(self, id_projection):
+        try:
+            return Projection.objects.get(id_projection=id_projection)
+
+        except Projection.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id_projection):
+        projection = Projection.objects.get(id_projection=id_projection)
+        now = dt.datetime.now()
+        time_range = dt.timedelta(days=14)
+        tickets = Seats.objects.filter(date__gte=(now - time_range), date__lte=(now + time_range), projection=projection)
+        how_many_tickets = tickets.count()
+
+        return Response({f'En un rango de 14 dias se vendieron {how_many_tickets} tickets en la proyeccion de la pelicula {projection.movie.name}'})
+
+
+# Reporte de ventas en peliculas activas
+
+class ReportActiveMovies(APIView):
+
+    def get(self, request):
+        try:
+            today = dt.date.today()
+            movies = Movie.objects.filter(state='ACTIVO')
+            show = []
+            for movie in movies:
+                how_many_tickets = 0
+                projections = Projection.objects.filter(movie=movie)
+                for projection in projections:
+                    seats = Seats.objects.filter(projection=projection, date__lte=today)
+                    how_many_tickets += seats.count()
+                
+                show.append({'Pelicula': movie.name, 'Tickets vendidos': how_many_tickets})
+
+            return Response(show)
+
+        except TypeError:
+            return JsonResponse({'Message': 'The query is wrong'}, status=status.HTTP_404_NOT_FOUND)
 
